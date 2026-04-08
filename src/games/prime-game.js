21e9a38c217601ad0e6b921d5130d4a7be9675f3
Dragon2124 <bogdanpytatin12345@gmail.com>
2026-04-08 13:24:40 +0700

    fix

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync'
 export const prime = (name) => {
     console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
     for (let i = 0; i < 3; i++) {
-        let number = Math.floor(Math.random() * (101 - 0) + 0) // NOSONAR
-        let correctAnswer
+        let number = Math.floor(Math.random() * (101 - 0) + 0)
+        let correctAnswer = 'yes'
 
         if (number !== 2) {
             if (number < 2 || number % 2 === 0) {
@@ -11,21 +11,23 @@ export const prime = (name) => {
             }
             else if (number % 2 !== 0 && number >= 3) {
                 const sqrt = Math.floor(Math.sqrt(number))
-                let j = 3
-                while (3 <= j && j < sqrt) {
-                    if (number % j === 0) {
+                let i = 3
+                while (3 <= i && i < sqrt) {
+                    if (number % i === 0) {
                         correctAnswer = 'no'
                         break
                     }
-                    else j += 2
+                    else i += 2
                 }
             }
         }
+
         console.log(`Question: ${number}`)
-        const answer = readlineSync.question('Your answer:')
+        const answer = readlineSync.question(`Your answer: `)
         if (answer === correctAnswer) {
             console.log('Correct!')
-        } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`)
+        }
+        else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`)
         if (i === 2) {
             console.log(`Congratulations, ${name}!`)
         }
