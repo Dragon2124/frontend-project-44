0c801627da7fab435d62ec2e116ec69b6ea2e9c1
Dragon2124 <bogdanpytatin12345@gmail.com>
2026-04-08 14:37:50 +0700

    add

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,12 +1,9 @@
 import checkAnswer from '../functions.js'
 export const prime = (name) => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
-
   for (let i = 0; i < 3; i += 1) {
-    // eslint-disable-next-line security/detect-math-random
     const number = Math.floor(Math.random() * 100) + 1
     let correctAnswer = 'yes'
-
     if (number < 2) {
       correctAnswer = 'no'
     }
@@ -18,7 +15,6 @@ export const prime = (name) => {
         }
       }
     }
-
     if (checkAnswer(number, correctAnswer, i, name) === false) {
       return
     }
